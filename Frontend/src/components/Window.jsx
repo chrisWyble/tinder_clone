4c4165a70b9c2eb525dbb8f597f4a9e77dc96a82
Chris Wyble <cwgiant@gmail.com>
2021-10-31 02:44:07 -0500

    Update sample structure

--- Frontend/src/components/Window.jsx
+++ Frontend/src/components/Window.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import profile001_1 from "sample/profile001/photos/6C59FeQfP5wNnTmAZuwLPU.jpg";
 
 class Window extends React.Component {
   render() {
     return (
       <div>
         <h1>Hey I am a window component</h1>
+        <div className="image">
+          <img src={profile001_1} alt="imageTest" />
+        </div>
+        <div className="buttonSection">
+          <div className="buttonGroup">
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
+        </div>
       </div>
     );
   }

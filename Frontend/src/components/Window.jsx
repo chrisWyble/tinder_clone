a2a3be17c1115483b758881b6d378765f077cdd5
Chris Wyble <cwgiant@gmail.com>
2021-10-30 23:59:57 -0500

    Fixes structure

---
+++ Frontend/src/components/Window.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Window extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hey I am a window component</h1>
+      </div>
+    );
+  }
+}
+
+export default Window;
